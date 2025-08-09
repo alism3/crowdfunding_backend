0b3d7af3cec19567b36527af3325d7cc7f3d56d2
alism3 <maiora.alice@gmail.com>
2025-08-09 13:03:35 +0800

    Create a new model

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Fundraiser(models.Model): #We inherit our class from the built-in models.Model that comes with Django
+    title = models.CharField(max_length=200) # the title field cannot be more than 200 characters long
+    description = models.TextField() 
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True) #date_created field should be automatically set with the current date when a new record is created.
+
+##In plain English, we have basically told Django this...
+
+#"Hey, make me a table in the database called Fundraiser, and give it the following columns:
+
+#title - this field should contain short strings of characters
+#description - this field should contain longer blocks of text
+#goal - this field should contain an integer
+#etc... (EVERYTIME I CREATE A NEW MODEL I HAVE TO MIGRATE)
