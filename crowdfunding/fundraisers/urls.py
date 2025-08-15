6ac1e2c85d880054c41134bb6c061b143a60f5ea
alism3 <maiora.alice@gmail.com>
2025-08-15 20:40:23 +0800

    Views

---
+++ crowdfunding/fundraisers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('fundraisers/', views.FundraiserList.as_view()),
+  path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
+ ]
+ #Now my new view can be accessed by adding fundraisers/ to the end of our back-end's address
