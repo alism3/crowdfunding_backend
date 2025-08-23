e621d4d05551bd3bf2885b987aa2ca3c0da90048
alism3 <maiora.alice@gmail.com>
2025-08-23 12:32:08 +0800

    Migrations updates and token

--- crowdfunding/fundraisers/urls.py
+++ crowdfunding/fundraisers/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
   path('fundraisers/', views.FundraiserList.as_view()),
   path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
-  path('pledges/', views.PledgeList.as_view())
- ]
- #Now my new view can be accessed by adding fundraisers/ to the end of our back-end's address
+  path('pledges/', views.PledgeList.as_view()) 
+  ] #Now my new view can be accessed by adding fundraisers/ to the end of our back-end's address
